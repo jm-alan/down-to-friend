6d9942f02b335b0ae5420a7fd2daf92dc35a70b2
Joshua <j.maxwell.alan@gmail.com>
2021-02-02 18:13:48 -0500

    event associations

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -10,8 +10,25 @@ module.exports = (sequelize, DataTypes) => {
     closes: DataTypes.DATE,
     tags: DataTypes.STRING
   }, {});
-  Event.associate = function(models) {
-    // associations can be defined here
+  Event.associate = function (models) {
+    const attendeeMap = {
+      as: 'Attendees',
+      through: models.Attendee,
+      foreignKey: 'eventId',
+      otherKey: 'userId'
+    };
+    const detailImageMap = {
+      as: 'DetailPhotos',
+      through: models.EventDetailImage,
+      foreignKey: 'eventId',
+      otherKey: 'imageId'
+    };
+    Event.hasMany(models.Attendee, { foreignKey: 'eventId' });
+    Event.belongsTo(models.User, { foreignKey: 'ownerId' });
+    Event.belongsToMany(models.User, attendeeMap);
+    Event.hasMany(models.EventDetailImage, { foreignKey: 'eventId' });
+    Event.belongsToMany(models.EventDetailImage, detailImageMap);
+    Event.hasMany(models.EventPost, { foreignKey: 'eventId' });
   };
   return Event;
 };
