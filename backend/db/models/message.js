0ae8afeaf00b70032a369899a9aa594ac6af7b4b
Joshua <j.maxwell.alan@gmail.com>
2021-02-02 18:22:38 -0500

    message associations

--- backend/db/models/message.js
+++ backend/db/models/message.js
@@ -5,8 +5,9 @@ module.exports = (sequelize, DataTypes) => {
     recipientId: DataTypes.INTEGER,
     content: DataTypes.TEXT
   }, {});
-  Message.associate = function(models) {
-    // associations can be defined here
+  Message.associate = function (models) {
+    Message.belongsTo(models.User, { as: 'Sender', foreignKey: 'senderId' });
+    Message.belongsTo(models.User, { as: 'Recipient', foreignKey: 'recipientId' });
   };
   return Message;
 };
