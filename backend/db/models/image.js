8c03d5152a20eec6daeaa66e107249282b71c2d9
Joshua <j.maxwell.alan@gmail.com>
2021-02-02 18:21:14 -0500

    image associations

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -3,8 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const Image = sequelize.define('Image', {
     url: DataTypes.STRING
   }, {});
-  Image.associate = function(models) {
-    // associations can be defined here
+  Image.associate = function (models) {
+    const detailImageMap = {
+      through: models.EventDetailImage,
+      foreignKey: 'imageId',
+      otherKey: 'eventId'
+    };
+    Image.belongsToMany(models.Event, detailImageMap);
   };
   return Image;
 };
