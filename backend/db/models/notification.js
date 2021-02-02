2abecc12d287bb68ffecef45f78e4a8a481c041c
Joshua <j.maxwell.alan@gmail.com>
2021-02-02 18:28:47 -0500

    notification associations

--- backend/db/models/notification.js
+++ backend/db/models/notification.js
@@ -4,8 +4,9 @@ module.exports = (sequelize, DataTypes) => {
     messageId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER
   }, {});
-  Notification.associate = function(models) {
-    
+  Notification.associate = function (models) {
+    Notification.belongsTo(models.Message, { foreignKey: 'messageId' });
+    Notification.belongsTo(models.User, { foreignKey: 'userId' });
   };
   return Notification;
 };
