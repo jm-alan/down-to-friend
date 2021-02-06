450ac2df6b002e268bc5d67c665f3641ebdd791d
Joshua <j.maxwell.alan@gmail.com>
2021-02-06 04:07:55 -0500

    psql is suddenly broken for everyone and no one knows why

--- backend/config/index.js
+++ backend/config/index.js
@@ -10,5 +10,11 @@ module.exports = {
   jwtConfig: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 };
