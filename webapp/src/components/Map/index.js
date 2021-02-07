256c609e21dd1e7869c378eb0330952335c6505c
Joshua <j.maxwell.alan@gmail.com>
2021-02-07 14:10:11 -0500

    rename Enumerate to EnumerateReel in anticipation of other similarly named functions to come; move map load watch into map component

--- webapp/src/components/Map/index.js
+++ webapp/src/components/Map/index.js
@@ -2,13 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import GoogleMap from 'google-map-react';
 
 import Pin from './Pin';
-import { Enumerate, UnloadReel, LoadReel, SetEnumerable } from '../../store/reel';
+import { EnumerateReel, UnloadReel, LoadReel, SetEnumerable } from '../../store/reel';
 import { Focus } from '../../store/map';
 
 export default function Map ({ list }) {
   const dispatch = useDispatch();
 
-  const { lat, lng, zoom } = useSelector(state => state.map);
+  const { lat, lng, zoom, loaded } = useSelector(state => state.map);
   const { enumerable } = useSelector(state => state.reel);
 
   const handleMapChange = ({ center, bounds, zoom: changeZoom }) => {
@@ -16,7 +16,7 @@ export default function Map ({ list }) {
       document.querySelectorAll('.map-pin')
         .forEach(pin => pin.classList.remove('focus'));
       dispatch(UnloadReel());
-      dispatch(Enumerate(
+      dispatch(EnumerateReel(
         center.lng,
         center.lat,
         Math.min(bounds.nw.lng, bounds.se.lng),
@@ -31,29 +31,31 @@ export default function Map ({ list }) {
     if (center.lat !== lat ||
       center.lng !== lng ||
       zoom !== changeZoom) {
-      dispatch(Focus(center.lng, center.lat, bounds, changeZoom));
+      dispatch(Focus(center.lng, center.lat, bounds, changeZoom || 10));
     }
     dispatch(SetEnumerable(true));
   };
 
-  return (
-    <div className='map-container'>
-      <GoogleMap
-        bootstrapURLKeys={{
-          key: process.env.REACT_APP_API_KEY
-        }}
-        center={{ lng, lat }}
-        zoom={zoom}
-        onChange={handleMapChange}
-      >{list && list.map(event => (
-        <Pin
-          event={event}
-          key={event.id}
-          lat={event.latitude}
-          lng={event.longitude}
-        />
-      ))}
-      </GoogleMap>
-    </div>
-  );
+  return loaded
+    ? (
+      <div className='map-container'>
+        <GoogleMap
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_API_KEY
+          }}
+          center={{ lng, lat }}
+          zoom={zoom}
+          onChange={handleMapChange}
+        >{list && list.map(event => (
+          <Pin
+            event={event}
+            key={event.id}
+            lat={event.latitude}
+            lng={event.longitude}
+          />
+        ))}
+        </GoogleMap>
+      </div>
+      )
+    : null;
 }
