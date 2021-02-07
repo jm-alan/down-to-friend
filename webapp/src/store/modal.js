ded4d36c58ea04cfeca0de27d1e8deff0083f2f1
Joshua <j.maxwell.alan@gmail.com>
2021-02-06 23:14:38 -0500

    add signup phase to modal reducer to switch between user creation and location lookup

--- webapp/src/store/modal.js
+++ webapp/src/store/modal.js
@@ -2,18 +2,24 @@ const FORM = 'modal/FORM';
 
 const DISPLAY = 'modal/DISPLAY';
 
+const PHASE = 'modal/PHASE';
+
 export const ModalForm = form => ({ type: FORM, form });
 
 export const ModalDisplay = display => ({ type: DISPLAY, display });
 
+export const SignupPhase = phase => ({ type: PHASE, phase });
+
 export default function reducer (
-  state = { form: 'login', display: false },
-  { type, form, display }) {
+  state = { form: 'login', display: false, phase: 1 },
+  { type, form, display, phase }) {
   switch (type) {
     case FORM:
       return { ...state, form };
     case DISPLAY:
       return { ...state, display };
+    case PHASE:
+      return { ...state, phase };
     default:
       return state;
   }
