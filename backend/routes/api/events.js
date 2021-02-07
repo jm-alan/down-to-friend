47efd9e2d12b01e81f03352c881733a4eebef8ae
Joshua <j.maxwell.alan@gmail.com>
2021-02-06 22:51:07 -0500

    modified events route to correctly ingest modified Enumerate query using explicit map bounds rather than calculated bounds; created user routes to both POST and GET user defaultLocale

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -6,19 +6,26 @@ const { User, Event, Image } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth');
 
 router.get('/', restoreUser, asyncHandler(async (req, res) => {
-  const { query: { longitude, latitude, lngDiff, latDiff } } = req;
-  if (!longitude || !latitude) return res.json({ success: false });
-  const lngSpread = +lngDiff / 2;
-  const latSpread = +latDiff / 2;
-  if (longitude) {
+  const {
+    query: {
+      centerLng,
+      centerLat,
+      lowerLng,
+      upperLng,
+      lowerLat,
+      upperLat
+    }
+  } = req;
+  if (!lowerLng || !upperLat) return res.json({ success: false });
+  if (lowerLng) {
     const localEvents = await Event.findAll({
       limit: 100,
       where: {
         longitude: {
-          [Op.between]: [+longitude - lngSpread, +longitude + lngSpread]
+          [Op.between]: [+lowerLng, +upperLng]
         },
         latitude: {
-          [Op.between]: [+latitude - latSpread, +latitude + latSpread]
+          [Op.between]: [+lowerLat, +upperLat]
         }
       },
       include: [
@@ -37,10 +44,10 @@ router.get('/', restoreUser, asyncHandler(async (req, res) => {
     });
     localEvents.sort((event1, event2) => {
       const event1distance = Math.sqrt(
-        (event1.longitude - longitude) ** 2 + (event1.latitude - latitude) ** 2
+        (event1.longitude - centerLng) ** 2 + (event1.latitude - centerLat) ** 2
       );
       const event2distance = Math.sqrt(
-        (event2.longitude - longitude) ** 2 + (event2.latitude - latitude) ** 2
+        (event2.longitude - centerLng) ** 2 + (event2.latitude - centerLat) ** 2
       );
       return event1distance - event2distance;
     });
