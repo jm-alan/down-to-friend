6c66699524c9d14a76ae53405bb47f6fd4f11ec1
Joshua <j.maxwell.alan@gmail.com>
2021-02-10 13:57:20 -0500

    create websocket requireAuth function

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,23 +1,20 @@
 const jwt = require('jsonwebtoken');
 const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const { User, Conversation } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
-// Sends a JWT Cookie
 const setTokenCookie = (res, user) => {
-  // Create the token.
   const token = jwt.sign(
     { data: user.toSafeObject() },
     secret,
-    { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    { expiresIn: parseInt(expiresIn) }
   );
 
   const isProduction = process.env.NODE_ENV === 'production';
 
-  // Set the token cookie
   res.cookie('token', token, {
-    maxAge: expiresIn * 1000, // maxAge in milliseconds
+    maxAge: expiresIn * 1000,
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction && 'Lax'
@@ -40,16 +37,38 @@ const restoreUser = (req, res, next) => {
       return next();
     }
 
-    if (!req.user) res.clearCookie('token');
+    if (!req.user) {
+      res.clearCookie('token');
+    }
 
     return next();
   });
 };
 
-// If there is no current user, return an error
+const socketRequireAuth = (socket, next) => {
+  socket.handshake.headers &&
+  socket.handshake.headers.cookie &&
+  jwt.verify(socket.handshake.headers.cookie.match(/(?<=; token=)(.*)(?=;)/)[0], secret, null, async (err, payload) => {
+    if (err) {
+      return socket.disconnect();
+    }
+
+    try {
+      const { id } = payload.data;
+      socket.user = await User.scope('currentUser').findByPk(id);
+    } catch (payloadErr) {
+      return socket.disconnect();
+    }
+
+    if (!socket.user) return socket.disconnect();
+
+    return next();
+  });
+};
+
 const requireAuth = [
   restoreUser,
-  function (req, res, next) {
+  function (req, _res, next) {
     if (req.user) return next();
 
     const err = new Error('Unauthorized');
@@ -60,4 +79,9 @@ const requireAuth = [
   }
 ];
 
-module.exports = { setTokenCookie, restoreUser, requireAuth };
+module.exports = {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+  socketRequireAuth
+};
