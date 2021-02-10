b6772d5226a4cbb0acb3d88db4f709ea03f5327a
Joshua <j.maxwell.alan@gmail.com>
2021-02-10 14:17:51 -0500

    create modal state and modify action creators to accept on-modal-unmount callback

--- webapp/src/store/modal.js
+++ webapp/src/store/modal.js
@@ -1,23 +1,34 @@
 const FORM = 'modal/FORM';
 
-const DISPLAY = 'modal/DISPLAY';
+const SHOW = 'modal/SHOW';
+
+const HIDE = 'modal/HIDE';
 
 const PHASE = 'modal/PHASE';
 
 export const ModalForm = form => ({ type: FORM, form });
 
-export const ModalDisplay = display => ({ type: DISPLAY, display });
+export const ShowModal = (after = null) => ({ type: SHOW, after });
+
+export const HideModal = () => ({ type: HIDE });
 
 export const SignupPhase = phase => ({ type: PHASE, phase });
 
 export default function reducer (
-  state = { form: 'login', display: false, phase: 1 },
-  { type, form, display, phase }) {
+  state = {
+    form: 'login',
+    display: false,
+    phase: 1,
+    after: null
+  },
+  { type, form, after, phase }) {
   switch (type) {
     case FORM:
       return { ...state, form };
-    case DISPLAY:
-      return { ...state, display };
+    case SHOW:
+      return { ...state, display: true, after };
+    case HIDE:
+      return { ...state, display: false, after: null };
     case PHASE:
       return { ...state, phase };
     default:
