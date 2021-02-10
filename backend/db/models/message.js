6f021f8f689db990777e27d358f3c895df465c32
Joshua <j.maxwell.alan@gmail.com>
2021-02-10 13:46:57 -0500

    messages are now bound exclusively to conversations and a sending user rather than a sender and recipient; consequently, messages no longer generate notifications, conversations do

--- backend/db/models/message.js
+++ backend/db/models/message.js
@@ -2,20 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define('Message', {
     senderId: DataTypes.INTEGER,
-    recipientId: DataTypes.INTEGER,
+    conversationId: DataTypes.INTEGER,
     content: DataTypes.TEXT
   }, {});
   Message.associate = function (models) {
-    const unreadMap = {
-      as: 'NotifiedUsers',
-      through: models.Notification,
-      foreignKey: 'messageId',
-      otherKey: 'userId'
-    };
     Message.belongsTo(models.User, { as: 'Sender', foreignKey: 'senderId' });
-    Message.belongsTo(models.User, { as: 'Recipient', foreignKey: 'recipientId' });
-    Message.hasMany(models.Notification, { foreignKey: 'messageId' });
-    Message.belongsToMany(models.User, unreadMap);
+    Message.belongsTo(models.Conversation, { foreignKey: 'conversationId' });
   };
   return Message;
 };
