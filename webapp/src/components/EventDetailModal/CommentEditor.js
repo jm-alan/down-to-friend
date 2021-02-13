0bc6ebbd2c59e7e6554e56c5dd4f4befe7bd1947
Joshua <j.maxwell.alan@gmail.com>
2021-02-13 03:01:00 -0500

    create comment edit form

---
+++ webapp/src/components/EventDetailModal/CommentEditor.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { EditComment } from '../../store/eventModal';
+
+export default function CommentEditor ({ comment, setEdit, hangHeight }) {
+  console.log(hangHeight);
+  const dispatch = useDispatch();
+  const event = useSelector(state => state.eventModal.event);
+
+  const [newComment, updateNewComment] = useState(comment.body);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newComment && newComment !== comment.body) {
+      dispatch(EditComment(event.id, comment.id, newComment))
+        .then(resp => {
+          if (resp.success) {
+            comment.body = newComment;
+            setEdit(false);
+          }
+        });
+    } else {
+      setEdit(false);
+    }
+  };
+
+  return (
+    <form
+      className='comment-edit'
+      onSubmit={onSubmit}
+    >
+      <textarea
+        style={{
+          height: `${hangHeight}px`
+        }}
+        required
+        type='text'
+        className='comment-edit-input'
+        value={newComment}
+        onChange={({ target: { value } }) => updateNewComment(value)}
+      />
+      <button className='comment-edit-submit'>
+        <i className='fas fa-check' />
+      </button>
+    </form>
+  );
+}
