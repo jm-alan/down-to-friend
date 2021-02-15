d58f9fb509ced0d0a893016b2a98a3afc1b6b8c8
Joshua <j.maxwell.alan@gmail.com>
2021-02-15 03:15:13 -0500

    create additional scops for high-traffic models, attach user Avatar to all instances

--- backend/db/models/conversation.js
+++ backend/db/models/conversation.js
@@ -3,7 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const Conversation = sequelize.define('Conversation', {
     name: DataTypes.STRING,
     createdBy: DataTypes.INTEGER
-  }, {});
+  }, {
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  });
   Conversation.associate = function (models) {
     const notifiedMap = {
       as: 'NotifiedUsers',
@@ -16,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       through: models.RosterEntry,
       foreignKey: 'conversationId',
       otherKey: 'userId'
-    }
+    };
     Conversation.hasMany(models.Message, { foreignKey: 'conversationId' });
     Conversation.hasMany(models.Notification, { foreignKey: 'conversationId' });
     Conversation.belongsTo(models.User, { as: 'ChatOwner', foreignKey: 'createdBy' });
