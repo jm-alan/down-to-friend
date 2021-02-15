5641ce5e05c2eab191f32e0615578c742d289f8c
Joshua <j.maxwell.alan@gmail.com>
2021-02-15 05:22:32 -0500

    implement state-based hot-load of new inbound/outbound messages

--- webapp/src/components/Messenger/ChatContainer.js
+++ webapp/src/components/Messenger/ChatContainer.js
@@ -1,11 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import MessagesContainer from './MessagesContainer';
 import InputBox from './InputBox';
 
-export default function ChatBox () {
+export default function ChatContainer () {
+  const socket = useSelector(state => state.messenger.socket);
+  const messages = useSelector(state => state.messenger.messages);
+  const [rollingMessages, updateRollingMessages] = useState(messages);
+
+  useEffect(() => {
+    socket.on('message', (content, user) => {
+      updateRollingMessages(rolling => [...rolling, {
+        Sender: user,
+        content
+      }]);
+    });
+  }, [socket]);
+
   return (
     <>
-      <MessagesContainer />
-      <InputBox />
+      <MessagesContainer
+        rollingMessages={rollingMessages}
+      />
+      <InputBox
+        updateRollingMessages={updateRollingMessages}
+      />
     </>
   );
 }
