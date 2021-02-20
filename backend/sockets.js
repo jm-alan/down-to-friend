fd1903dc75a087017a7ef6e69b1087a874d48fe7
Joshua <j.maxwell.alan@gmail.com>
2021-02-19 20:10:51 -0500

    abstract per-conversation logic into reusable function; create method to attach that per-conversation logic to new conversations as they are created

--- backend/sockets.js
+++ backend/sockets.js
@@ -1,9 +1,79 @@
 const io = require('socket.io')();
 
+const { Conversation } = require('./db/models');
 const { socketRequireAuth } = require('./utils/auth');
 
 const roomMap = {};
 const liveUsers = new Set();
+
+const convoLogic = (socket, user, convos) => convo => {
+  // frontend emits 'viewing-#' event when an individual
+  // conversation is selected.
+  socket.on(`viewing-${convo.id}`, () => {
+    // iterate over the previously enumerated conversations,
+    // looking up their rooms in the room map and deleting this
+    // user from their viewers, enabling this user to be notified
+    // of new messages in that conversation.
+    convos.forEach(({ id }) => {
+      roomMap[id] && roomMap[id].delete(user.id);
+    });
+    // if this room has never been viewed before (either it's a
+    // new conversation, or the server has restarted since the last
+    // time anyone viewed it), create a new room participant tracker
+    roomMap[convo.id] ?? (roomMap[convo.id] = new Set());
+    roomMap[convo.id].add(user.id);
+  });
+  // Join the socket room! Socket.IO will automatically create it
+  // if it doesn't exist. Like magic!
+  socket.join(convo.id);
+  // Frontend emits conversation-specific events on message
+  socket.on(`convo-${convo.id}`, (content) => {
+    // assume all messages coming in from authenticated socket are
+    // created by the user to whom that authentication belongs;
+    // anything else would require cracking JWT secret which is
+    // particularly unlikely.
+    user.createSentMessage({
+      conversationId: convo.id,
+      content
+    })
+      // only emit new message event after message has been created
+      // in database.
+      .then(({ content }) => {
+        socket.to(convo.id).emit(`convo-${convo.id}`, content, user);
+      });
+    convo.getChattingUsers()
+      .then(users => {
+        const toNotify = [];
+        users.forEach(chattingUser => {
+          // If a user is not currently viewing a room, create a notif
+          // in the database regardless of whether they're online.
+          if (
+            roomMap[convo.id] &&
+            (!roomMap[convo.id].has(chattingUser.id))
+          ) {
+            toNotify.push(chattingUser.id);
+            chattingUser.createNotification({
+              conversationId: convo.id
+            });
+          }
+        });
+        toNotify.forEach(userId => {
+          // If and only if a user is actively online, also emit a live
+          // notification event letting them know they've received a
+          // message.
+          if (liveUsers.has(userId)) {
+            io.to(`notif-${userId}`).emit(`convo-notif-${convo.id}`);
+          }
+        });
+      });
+  });
+  // If a socket disconnects, it's physically impossible for that user
+  // to be actively viewing the room; remove them from the viewing user
+  // list.
+  socket.on('disconnect', () => {
+    roomMap[convo.id] && roomMap[convo.id].delete(user.id);
+  });
+};
 
 io.use(socketRequireAuth)
   .on('connection', (socket) => {
@@ -16,73 +86,22 @@ io.use(socketRequireAuth)
         user.getChats()
           .then(convos => {
             // this will always be an array, just sometimes it will be empty.
-            convos.forEach(convo => {
-              // frontend emits 'viewing-#' event when an individual
-              // conversation is selected.
-              socket.on(`viewing-${convo.id}`, () => {
-                // iterate over the previously enumerated conversations,
-                // looking up their rooms in the room map and deleting this
-                // user from their viewers, enabling this user to be notified
-                // of new messages in that conversation.
-                convos.forEach(({ id }) => {
-                  if (
-                    roomMap[id] &&
-                    roomMap[id].has(user.id)
-                  ) roomMap.delete(user.id);
+            // This convoLogic function was abstracted to enable easily adding
+            // new conversations
+            convos.forEach(convoLogic(socket, user, convos));
+            socket.on('new', (convoId) => {
+              Conversation.findByPk(convoId, {
+                include: ['ChattingUsers']
+              })
+                .then(convo => {
+                  convo.getChattingUsers()
+                    .then(users => {
+                      users.forEach(({ id }) => {
+                        socket.to(`notif-${id}`).emit(`convo-notif-${convo.id}`);
+                      });
+                    });
+                  convoLogic(socket, user, convos)(convo);
                 });
-                // if this room has never been viewed before (either it's a
-                // new conversation, or the server has restarted since the last
-                // time anyone viewed it, create a new room participant tracker
-                if (!roomMap[convo.id]) roomMap[convo.id] = new Set();
-                roomMap[convo.id].add(user.id);
-              });
-              // Join the socket room! Socket.IO will automatically create it
-              // if it doesn't exist. Like magic!
-              socket.join(convo.id);
-              // Frontend emits conversation-specific events on message
-              socket.on(`convo-${convo.id}`, (content) => {
-                // assume all messages coming in from authenticated socket are
-                // created by the user to whom that authentication belongs;
-                // anything else would require cracking JWT secret which is
-                // particularly unlikely.
-                user.createSentMessage({
-                  conversationId: convo.id,
-                  content
-                })
-                  // only emit new message event after message has been created
-                  // in database.
-                  .then(({ content }) => {
-                    socket.to(convo.id).emit(`convo-${convo.id}`, content, user);
-                  });
-                convo.getChattingUsers()
-                  .then(users => {
-                    const toNotify = [];
-                    users.forEach(chattingUser => {
-                      // If a user is not currently viewing a room, create a notif
-                      // in the database regardless of whether they're online.
-                      if (!roomMap[convo.id].has(chattingUser.id)) {
-                        toNotify.push(chattingUser.id);
-                        chattingUser.createNotification({
-                          conversationId: convo.id
-                        });
-                      }
-                    });
-                    toNotify.forEach(userId => {
-                      // If and only if a user is actively online, also emit a live
-                      // notification event letting them know they've received a
-                      // message.
-                      if (liveUsers.has(userId)) {
-                        io.to(`notif-${userId}`).emit(`convo-notif-${convo.id}`);
-                      }
-                    });
-                  });
-              });
-              // If a socket disconnects, it's physically impossible for that user
-              // to be actively viewing the room; remove them from the viewing user
-              // list.
-              socket.on('disconnect', () => {
-                roomMap[convo.id].delete(user.id);
-              });
             });
           });
         break;
