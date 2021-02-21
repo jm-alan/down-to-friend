c2cd5ea08e536260bc90475ff9eeca472a51c31f
Joshua <j.maxwell.alan@gmail.com>
2021-02-21 00:30:28 -0500

    make home button refocus map; this has the dual function of giving the user the ability to quickly return the map to their home locale, and cleaning up any leftover map state that may accidentally persist from nonsequitur navigation events

--- webapp/src/components/Navigation/index.js
+++ webapp/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, NavLink } from 'react-router-dom';
 
 import ProfileButton from './ProfileButton';
@@ -7,16 +7,28 @@ import FormModal from '../FormModal';
 import SearchBar from '../SearchBar';
 import EventDetailModal from '../EventDetailModal';
 import AboutMe from '../AboutMe';
+import { Focus } from '../../store/map';
+import { GetLocale } from '../../store/user';
 
 import './Navigation.css';
 
 export default function Navigation () {
+  const dispatch = useDispatch();
   const location = useLocation();
   const user = useSelector(state => state.session.user);
   const event = useSelector(state => state.eventModal.event);
   const sessionLoaded = useSelector(state => state.session.loaded);
 
   const isHome = location.pathname.match(/^\/$/);
+
+  const homeClick = () => {
+    if (user) {
+      dispatch(GetLocale())
+        .then(({ lng, lat }) => {
+          dispatch(Focus(lng, lat, null, 10));
+        });
+    } else return dispatch(Focus(-121.49428149672518, 38.57366700738277, null, 10));
+  };
 
   return (
     <nav className='navbar'>
@@ -30,7 +42,10 @@ export default function Navigation () {
               <>
                 <ProfileButton />
                 <NavLink to='/'>
-                  <button className='nav-button home'>
+                  <button
+                    className='nav-button home'
+                    onClick={homeClick}
+                  >
                     Home
                   </button>
                 </NavLink>
