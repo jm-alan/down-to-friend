7ad7e6830777aa1a2ea4677405aeee135672ae26
Joshua <j.maxwell.alan@gmail.com>
2021-02-25 08:31:33 -0500

    create algorithm to dynamically reduce number of pins displayed on map per user input

--- webapp/src/components/Map/index.js
+++ webapp/src/components/Map/index.js
@@ -5,14 +5,40 @@ import Pin from './Pin';
 import { EnumerateReel, UnloadReel, LoadReel, SetEnumerable } from '../../store/reel';
 import { Focus } from '../../store/map';
 
-export default function Map ({ list }) {
+export default function Map ({ list: totalList }) {
   const dispatch = useDispatch();
 
-  const { lat, lng, zoom, loaded, fixed } = useSelector(state => state.map);
+  const pins = useSelector(state => state.map.pins);
   const enumerable = useSelector(state => state.reel.enumerable);
+  const sliderMode = useSelector(state => state.homeSlider.mode);
+  const { lat, lng, zoom, loaded, fixed } = useSelector(state => state.map);
+
+  const ratio = totalList && (totalList.length / pins);
+
+  const reducedList = totalList && (pins >= totalList.length
+    ? totalList
+    : totalList.filter((_item, idx) => !(idx % Math.ceil(ratio))));
+
+  reducedList && (
+    (
+      reducedList.length < pins && totalList.length >= pins
+    ) && (
+      (
+        (
+          reducedList[reducedList.length - 1].id !== totalList[totalList.length - 1].id
+        ) && (
+          reducedList.push(totalList[totalList.length - 1])
+        )
+      ) || reducedList.push(totalList[totalList.length - 2])
+    )
+  );
 
   const handleMapChange = ({ center, bounds, zoom: changeZoom }) => {
-    if (enumerable && !fixed) {
+    if (
+      sliderMode === 'reel' &&
+      enumerable &&
+      !fixed
+    ) {
       document.querySelectorAll('.map-pin')
         .forEach(pin => pin.classList.remove('focus'));
       dispatch(UnloadReel());
@@ -28,9 +54,11 @@ export default function Map ({ list }) {
           dispatch(LoadReel());
         });
     }
-    if (center.lat !== lat ||
+    if (
+      center.lat !== lat ||
       center.lng !== lng ||
-      zoom !== changeZoom) {
+      zoom !== changeZoom
+    ) {
       dispatch(Focus(center.lng, center.lat, bounds, changeZoom || 10));
     }
     dispatch(SetEnumerable(true));
@@ -46,7 +74,7 @@ export default function Map ({ list }) {
           center={{ lng, lat }}
           zoom={zoom}
           onChange={handleMapChange}
-        >{list && list.map(event => (
+        >{reducedList && reducedList.map(event => (
           <Pin
             event={event}
             key={event.id}
