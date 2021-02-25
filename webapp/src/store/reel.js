80d7de91b6e2e96b024ce810e7457905e094347f
Joshua <j.maxwell.alan@gmail.com>
2021-02-25 08:39:46 -0500

    correct bug with pin hide/restore during new event creation

--- webapp/src/store/reel.js
+++ webapp/src/store/reel.js
@@ -2,6 +2,8 @@ import csrfetch from './csrf';
 
 const ENUMERATE = 'reel/ENUMERATE';
 
+const HARDSET = 'reel/HARDSET';
+
 const RESTORE = 'reel/RESTORE';
 
 const MODE = 'reel/MODE';
@@ -14,7 +16,7 @@ const REF = 'reel/REF';
 
 const enumerate = list => ({ type: ENUMERATE, list });
 
-export const HardSetList = pin => ({ type: ENUMERATE, list: pin ?? [] });
+export const HardSetList = pin => ({ type: HARDSET, list: pin ?? [] });
 
 export const RestoreList = () => ({ type: RESTORE });
 
@@ -48,21 +50,20 @@ export const EnumerateReel = (
   dispatch(enumerate(data.list));
 };
 
-const reducer = (state = {
-  list: null,
-  loaded: false,
-  searchCenter: { lat: 0, lng: 0 },
-  enumerable: true,
-  ref: null,
-  store: []
-}, { type, list, enumerable, ref }) => {
+export default function reducer (
+  state = {
+    list: null,
+    loaded: false,
+    searchCenter: { lat: 0, lng: 0 },
+    enumerable: true,
+    ref: null,
+    store: []
+  }, { type, list, enumerable, ref }) {
   switch (type) {
     case ENUMERATE:
-      return {
-        ...state,
-        store: state.list ?? [],
-        list
-      };
+      return { ...state, list };
+    case HARDSET:
+      return { ...state, store: (!state.store.length && state.list) || state.store, list };
     case LOAD:
       return { ...state, loaded: true };
     case UNLOAD:
@@ -70,16 +71,10 @@ const reducer = (state = {
     case MODE:
       return { ...state, enumerable };
     case RESTORE:
-      return {
-        ...state,
-        store: [],
-        list: [...state.store]
-      };
+      return { ...state, store: [], list: [...state.store] };
     case REF:
       return { ...state, ref };
     default:
       return state;
   }
-};
-
-export default reducer;
+}
