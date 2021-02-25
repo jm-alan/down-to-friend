7bb893eee88367a53cf273770436753476750781
Joshua <j.maxwell.alan@gmail.com>
2021-02-25 08:27:11 -0500

    create search settings page for logged in users

---
+++ webapp/src/components/Slider/SearchSettings.js
@@ -0,0 +1,135 @@
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Debouncer from '../../utils/Debouncer';
+import { SetPins } from '../../store/map';
+import { UpdateSearchSettings, SetLocale } from '../../store/user';
+
+const pinSetter = (dispatch, setShowSaving1, pins) => {
+  dispatch(SetPins(pins));
+  dispatch(UpdateSearchSettings(pins))
+    .then(success => {
+      success && setShowSaving1(false);
+    });
+};
+
+const debouncedPinSetter = Debouncer(pinSetter, 250);
+
+export default function SearchSettings () {
+  const dispatch = useDispatch();
+
+  const user = useSelector(state => state.session.user);
+  const { lng, lat } = useSelector(state => state.map);
+
+  const [maxPins, setMaxPins] = useState(1);
+  const [showWarning, setShowWarning] = useState(false);
+  const [showSaving1, setShowSaving1] = useState(false);
+  const [showSaving2, setShowSaving2] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const onChangeMaxPins = ({ target: { value } }) => {
+    setShowSaving1(true);
+    setShowSaving2(true);
+    value.match(/^[0-9]+$/) && setMaxPins(value);
+    value || setMaxPins(1);
+    debouncedPinSetter(dispatch, setShowSaving1, value || 1);
+    ((value > 199) && (
+      setShowWarning(true) ?? true
+    )) || setShowWarning(false);
+  };
+
+  useEffect(() => {
+    user && dispatch(SetPins(user.maxPins)) && setMaxPins(user.maxPins);
+    user ?? (dispatch(SetPins(25)) && setMaxPins(25));
+  }, [dispatch, user]);
+
+  useEffect(() => {
+    showSaving2 && setTimeout(() => {
+      setShowSaving2(false);
+    }, 500);
+  }, [showSaving2]);
+
+  const setLocale = () => {
+    setShowSaving1(true);
+    setShowSaving2(true);
+    dispatch(SetLocale({ lng, lat }))
+      .then(() => {
+        setShowSaving1(false);
+      });
+  };
+
+  const inputBoundingRect = inputRef.current && inputRef.current.getBoundingClientRect();
+
+  return (
+    <div className='slider-form-container searchsettings'>
+      <form className='slider-form search-preferences'>
+        <div className='max-events-display-container'>
+          <div className='input-container max-events'>
+            <span className='pin-limit-label-wrapper'>
+              Limit the number of pins displayed on the map
+            </span>
+            <div className='input-and-button-container'>
+              <button
+                type='button'
+                className='plus'
+              >
+                <i className='fas fa-plus' />
+              </button>
+              <input
+                type='text'
+                value={maxPins}
+                onChange={onChangeMaxPins}
+                onBlur={() => setShowWarning(false)}
+                min={1}
+                ref={inputRef}
+              />
+              <button
+                type='button'
+                className='minus'
+              >
+                <i className='fas fa-minus' />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='set-locale-display-container'>
+          <button
+            className='set-locale-button'
+            type='button'
+            onClick={setLocale}
+          >
+            Set my default locale to the current map center
+          </button>
+        </div>
+        {showSaving1 || showSaving2
+          ? (
+            <div
+              className='saving-animation-container'
+            >
+              <img
+                src={`${process.env.PUBLIC_URL}/img/dual-ring-small.svg`}
+                alt=''
+              /> Saving...
+            </div>
+            )
+          : null}
+      </form>
+      {showWarning
+        ? (
+          <div
+            className='max-pins-warning-container'
+            style={inputBoundingRect && {
+              top: inputBoundingRect.top - 150,
+              left: inputBoundingRect.left
+            }}
+          >
+            <div className='max-pins-warning'>
+              Setting this too high can impact map performance!
+            </div>
+          </div>
+          )
+        : null}
+    </div>
+  );
+}
