194549ccb7a38cbc0c0d17e5bffc2dec0603cba8
Joshua <j.maxwell.alan@gmail.com>
2021-02-26 00:13:08 -0500

    remove unused functions

--- webapp/src/context/Modal.js
+++ webapp/src/context/Modal.js
@@ -1,8 +1,6 @@
 import { createContext, useContext, useRef, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ReactDOM from 'react-dom';
-
-import { LoadSession } from '../store/session';
 
 import './Modal.css';
 
