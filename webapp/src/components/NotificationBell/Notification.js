90796b868bc7322ce71b27f6b7ad8f809b84000a
Joshua <j.maxwell.alan@gmail.com>
2021-03-06 19:17:09 -0500

    Abstract QuickReply into separate component; praxis

---
+++ webapp/src/components/NotificationBell/Notification.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import QuickReply from './QuickReply';
+import { AddLock, RemoveLock } from '../../store/notifManager';
+
+export default function Notification ({ notif, setExpandContainer }) {
+  const dispatch = useDispatch();
+  const notifLock = useSelector(state => state.notifManager.lock);
+
+  const [showQuickReply, setShowQuickReply] = useState(false);
+  const [expandMessage, setExpandMessage] = useState(false);
+
+  return (
+    <div
+      key={notif.id}
+      className='notif-container'
+      onClick={({ target }) => {
+        if (
+          (
+            target.className.match(/notif-container/) ||
+            target.className.match(/notif-sender-container/) ||
+            target.className.match(/notif-content-container/)
+          ) && showQuickReply === false
+        ) {
+          dispatch(AddLock());
+          setShowQuickReply(true);
+        } else {
+          dispatch(RemoveLock());
+          setShowQuickReply(false);
+        }
+      }}
+      onMouseEnter={() => {
+        setExpandMessage(true);
+        setExpandContainer(true);
+      }}
+      onMouseLeave={() => {
+        !showQuickReply && setExpandMessage(false);
+        !notifLock && setExpandContainer(false);
+      }}
+      style={{
+        height: expandMessage
+          ? showQuickReply ? 175 : 135
+          : 60
+      }}
+    >
+      <div className='notif-sender-container'>
+        {notif.sender}
+      </div>
+      <div
+        className='notif-content-container'
+        style={{
+          whiteSpace: expandMessage ? 'normal' : 'nowrap',
+          overflow: expandMessage ? 'auto' : 'hidden',
+          textOverflow: expandMessage ? 'unset' : 'ellipsis',
+          minHeight: expandMessage ? 100 : 'unset'
+        }}
+      >
+        {notif.content}
+      </div>
+      <QuickReply
+        notif={notif}
+        showQuickReply={showQuickReply}
+        setShowQuickReply={setShowQuickReply}
+        setExpandMessage={setExpandMessage}
+      />
+    </div>
+  );
+}
---
+++ webapp/src/components/NotificationBell/QuickReply.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { GetNotifications, ClearNotif } from '../../store/notifManager';
+
+export default function QuickReply ({ notif, showQuickReply, setShowQuickReply, setExpandMessage }) {
+  const dispatch = useDispatch();
+  const notifSocket = useSelector(state => state.notifManager.socket);
+
+  const [quickReply, updateQuickReply] = useState('');
+
+  const reply = (e, notif) => {
+    e.preventDefault();
+    notifSocket.emit('quickReply', notif.conversationId, quickReply);
+    updateQuickReply('');
+    dispatch(ClearNotif(notif.conversationId))
+      .then(() => {
+        setShowQuickReply(false);
+        setExpandMessage(false);
+        dispatch(GetNotifications());
+      });
+  };
+
+  return showQuickReply
+    ? (
+      <form
+        className='quick-reply-container'
+        style={{
+          bottom: showQuickReply ? 0 : -45
+        }}
+        onSubmit={(e) => reply(e, notif)}
+      >
+        <input
+          type='text'
+          placeholder='Reply...'
+          value={quickReply}
+          onChange={({ target: { value } }) => updateQuickReply(value)}
+        />
+        <button className='quick-reply-submit'>
+          <i className='fas fa-paper-plane' />
+        </button>
+      </form>
+      )
+    : null;
+}
