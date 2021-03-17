7ee752bffdc68a94ebf124d3af5034e282698022
Joshua <j.maxwell.alan@gmail.com>
2021-03-17 09:11:34 -0400

    create notification socket handshake to improve connection reliability for idle sockets

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -32,19 +32,30 @@ io.use(socketRequireAuth)
           });
         break;
       case 'notif':
-        // Notification contains inline reply functionality
-        socket.on('quickReply', onQuickReply);
-        // Create a room just for this user's notifications to be received.
-        socket.join(`notif-${userId}`);
-        // Add the user to the running list of all connected users.
-        liveUsers.add(userId);
+        // Manually emit handshake event so socket is not idle; vastly improves
+        // socket connection reliability in cases where notifications are not
+        // actively being emitted when a user logs in.
+        io.to(socket.id).emit('handshake');
+        // Wait for socket to confirm handshake before instantiating logic
+        socket.on('handshake', () => {
+          // Notification contains inline reply functionality
+          socket.on('quickReply', onQuickReply);
+          // Create a room just for this user's notifications to be received.
+          socket.join(`notif-${userId}`);
+          // Add the user to the running list of all connected users.
+          liveUsers.add(userId);
+        });
         // On disconnect, remove them from the runing list of connected users.
+        // Keep this outisde the handshake logic to be 100% certain liveUsers
+        // does not contain userIds that don't have notification sockets listening,
+        // even if something goes wrong with the handshake process. Better safe than
+        // sorry.
         socket.on('disconnect', () => {
           liveUsers.delete(userId);
         });
         break;
       default:
-        // If a correct type is not supplied by the socket query, it is roque;
+        // If a correct type is not supplied by the socket query, it is rogue;
         // force disconnect and close the underlying connection.
         return socket.disconnect(true);
     }
