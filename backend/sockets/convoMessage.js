c41755edb13b8eeab09d11c00e2ff528e0af557d
JM <j.maxwell.alan@gmail.com>
2021-10-27 02:07:03 -0400

    refactor convoMessage as async IIFE

--- backend/sockets/convoMessage.js
+++ backend/sockets/convoMessage.js
@@ -1,41 +1,39 @@
 module.exports = (io, socket, Notification, roomMap, liveUsers, user, convo) => content => {
-  const { id: conversationId } = convo;
-  // assume all messages coming in from authenticated socket are
-  // created by the user to whom that authentication belongs;
-  // anything else would require cracking JWT secret which is
-  // particularly unlikely.
-  user.createSentMessage({ conversationId, content })
-  // only emit new message event after message has been created
-  // in database.
-    .then(({ content }) => {
-      socket.to(conversationId).emit(`convo-${conversationId}`, content, user);
+  (async () => {
+    const { id: conversationId } = convo;
+    // assume all messages coming in from authenticated socket are
+    // created by the user to whom that authentication belongs;
+    // anything else would require cracking JWT secret which is
+    // particularly unlikely.
+    const newMessage = await user.createSentMessage({ conversationId, content });
+    socket.to(conversationId).emit(`convo-${conversationId}`, newMessage.content, user);
+    const users = await convo.getChattingUsers();
+    const toNotify = [];
+    await Notification.destroy({
+      where: { conversationId }
     });
-  convo.getChattingUsers()
-    .then(users => {
-      const toNotify = [];
-      Notification.destroy({
-        where: { conversationId }
-      }).then(() => {
-        users.forEach(chattingUser => {
-          // If a user is not currently viewing a room, create a notif
-          // in the database regardless of whether they're online.
-          if (
-            roomMap[conversationId] &&
-              (!roomMap[conversationId].has(chattingUser.id))
-          ) {
-            toNotify.push(chattingUser.id);
-            chattingUser.createNotification({ conversationId });
-          }
-        });
-        toNotify.forEach(userId => {
-          // If and only if a user is actively online, also emit a live
-          // notification event letting them know they've received a
-          // message.
-          if (liveUsers.has(userId)) {
-            io.to(`notif-${userId}`)
-              .emit('chat', { sender: user.firstName, content, conversationId });
-          }
-        });
+    // If the roommap does not exist, then no users are viewing this
+    // conversation, and we can simply notify every user.
+    if (!roomMap[conversationId]) {
+      toNotify.push(...users.map(({ id }) => id));
+    } else {
+      users.forEach(chattingUser => {
+      // If a user is not currently viewing a room, create a notif
+      // in the database regardless of whether they're online.
+        if (!roomMap[conversationId].has(chattingUser.id)) {
+          toNotify.push(chattingUser.id);
+          chattingUser.createNotification({ conversationId });
+        }
       });
+    }
+    toNotify.forEach(userId => {
+      // If and only if a user is actively online, also emit a live
+      // notification event letting them know they've received a
+      // message.
+      if (liveUsers.has(userId)) {
+        io.to(`notif-${userId}`)
+          .emit('chat', { sender: user.firstName, content, conversationId });
+      }
     });
+  })();
 };
