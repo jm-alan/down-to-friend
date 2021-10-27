f26d340645736d2e3f1f2ccda00c67092234c322
JM <j.maxwell.alan@gmail.com>
2021-10-27 02:25:47 -0400

    create dedicated prototypes file

---
+++ backend/utils/prototypes.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-extend-native */
+Array.prototype.asyncForEach = async function ($) {
+  for (let _ = 0; _ < this.length; _++) {
+    await $(this[_], _, this);
+  }
+};
+
+Array.prototype.asyncMapInPlace = async function ($) {
+  for (let _ = 0; _ < this.length; _++) {
+    this[_] = await $(this[_], _, this);
+  }
+};
+
+Array.prototype.toKeyedObject = function (key) {
+  return this.reduce((acc, next) => (acc[next[key]] = next) && acc, {});
+};
