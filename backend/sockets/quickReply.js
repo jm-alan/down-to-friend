45a7a89974d7a6c349159cc66c3e59afb7dbbab9
JM <j.maxwell.alan@gmail.com>
2021-10-27 02:14:14 -0400

    refactor quickReply as async IIFE

--- backend/sockets/quickReply.js
+++ backend/sockets/quickReply.js
@@ -1,41 +1,35 @@
 module.exports = (io, socket, Notification, Conversation, roomMap, liveUsers, user) => (conversationId, content) => {
-  const { id: userId } = user;
-  user.createSentMessage({
-    conversationId,
-    content
-  })
-    .then(({ content }) => {
-      socket.to(conversationId).emit(`convo-${conversationId}`, content, user);
+  (async () => {
+    const { id: userId } = user;
+    const newMessage = await user.createSentMessage({
+      conversationId,
+      content
     });
-  Conversation.findByPk(conversationId)
-    .then(convo => {
-      const { id: conversationId } = convo;
-      convo.getChattingUsers()
-        .then(users => {
-          const toNotify = [];
-          Notification.destroy({
-            where: {
-              conversationId
-            }
-          }).then(() => {
-            users.forEach(chattingUser => {
-              if (
-                roomMap[convo.id] &&
-                (!roomMap[convo.id].has(chattingUser.id)) &&
-                chattingUser.id !== userId
-              ) {
-                toNotify.push(chattingUser.id);
-                chattingUser.createNotification({
-                  conversationId
-                });
-              }
-            });
-            toNotify.forEach(userId => {
-              if (liveUsers.has(userId)) {
-                io.to(`notif-${userId}`).emit('chat', { sender: user.firstName, content, conversationId });
-              }
-            });
-          });
-        });
+    socket.to(conversationId).emit(`convo-${conversationId}`, newMessage.content, user);
+    const convo = await Conversation.findByPk(conversationId);
+    const users = convo.getChattingUsers();
+    const toNotify = [];
+    await Notification.destroy({
+      where: {
+        conversationId
+      }
     });
+    users.forEach(chattingUser => {
+      if (
+        roomMap[convo.id] &&
+        !roomMap[convo.id].has(chattingUser.id) &&
+        chattingUser.id !== userId
+      ) {
+        toNotify.push(chattingUser.id);
+        chattingUser.createNotification({ conversationId });
+      }
+    });
+    toNotify.forEach(userId => {
+      if (liveUsers.has(userId)) {
+        io
+          .to(`notif-${userId}`)
+          .emit('chat', { sender: user.firstName, content, conversationId });
+      }
+    });
+  })();
 };
